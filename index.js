fae01c2cc4e2ac317dc5f8b79e1a2860886ac66d
TharinduDeshananda <tdeshananda@gmail.com>
2022-04-15 16:36:34 +0530

    menu added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let plusSign = document.querySelector(".overlay");
 let menuBackground = document.querySelector(".menu-background");
+let menu = document.querySelector(".overlay-menu");
 plusSign.addEventListener("click", () => {
   plusSign.classList.toggle("active");
   menuBackground.classList.toggle("active");
+  menu.classList.toggle("active");
 });
